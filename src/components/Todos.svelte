b781da51b475ada4415f43338ca925397bacfdc3
matthewberzinskas <pasomatt@gmail.com>
2022-04-20 11:06:38 -0700

    Componentization of Filter

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,4 +1,5 @@
 <script>
+  import FilterButton from "./FilterButton.svelte";
   export let todos = [];
 
   $: totalTodos = todos.length;
@@ -54,38 +55,7 @@
   </form>
 
   <!-- Filter -->
-  <div class="filters btn-group stack-exception">
-    <button
-      class="btn toggle-btn"
-      class:btn_primary={filter === "all"}
-      aria-pressed={filter === "all"}
-      on:click={() => (filter = "all")}
-    >
-      <span class="visually-hidden">Show</span>
-      <span>All</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button
-      class="btn toggle-btn"
-      class:btn_primary={filter === "active"}
-      aria-pressed={filter === "active"}
-      on:click={() => (filter = "active")}
-    >
-      <span class="visually-hidden">Show</span>
-      <span>Active</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button
-      class="btn toggle-btn"
-      class:btn_primary={filter === "completed"}
-      aria-pressed={filter === "completed"}
-      on:click={() => (filter = "completed")}
-    >
-      <span class="visually-hidden">Show</span>
-      <span>Completed</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-  </div>
+  <FilterButton bind:filter />
 
   <!-- TodosStatus -->
   <h2 id="list-heading">
