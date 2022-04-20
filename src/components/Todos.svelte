7b8abdc5bd66496a23c2f23cd4ef73e99d9b818c
matthewberzinskas <pasomatt@gmail.com>
2022-04-19 21:45:09 -0700

    Adding static markup

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,1 +1,46 @@
 <h1>Svelte to-do list</h1>
+
+<!-- Todos.svelte -->
+<div class="todoapp stack-large">
+
+  <!-- Form for new ToDo Task -->
+  <form>
+    <h2 class="label-wrapper">
+      <label for="todo-0" class="label__lg">
+        What needs to be done?
+      </label>
+    </h2>
+
+    <input type="text" id="todo-0" autocomplete="off" class="inpute inpute__lg"/>
+    <button type="submit" disabled="" class="btn btn__primary btn__lg">
+      Add
+    </button>
+  </form>
+
+  <!-- Filter element for ToDo Tasks -->
+  <div class="filters btn-group stack-exception">
+    <button class="btn toggle-btn" aria-pressed="true">
+      <span class="visually-hidden">Show</span>
+      <span>All</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+
+    <button class="btn toggle-btn" aria-pressed="false">
+      <span class="visually-hidden">Show</span>
+      <span>Active</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+
+    <button class="btn toggle-btn" aria-pressed="true">
+      <span class="visually-hidden">Show</span>
+      <span>Completed</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+  </div>
+
+  <!-- Status of ToDo Tasks-->
+
+  <!-- ToDo Tasks -->
+
+  
+</div>
